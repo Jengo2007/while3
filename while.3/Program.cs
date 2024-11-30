5a737d22a704d2964fab14490fb4824e182cbf56
Jengo2007 <zuyunovmagamed@gmail.com>
2024-11-30 19:14:10 +0300

    while3

---
+++ while.3/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int a =int.Parse(Console.ReadLine());
+int b =int.Parse(Console.ReadLine());
+double count = 0;
+int result =a;
+
+while (result >= b)
+{
+    
+    
+        result-= b;
+        count++;
+        
+
+
+
+
+
+
+}
+
+Console.WriteLine(result);
+Console.WriteLine(count);
